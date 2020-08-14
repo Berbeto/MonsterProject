629e37d415093692659d3c767f8f037f136766ea
Wojciech Jarosz <jaroszw@gmail.com>
2020-08-14 10:10:04 +0200

    Updates

---
+++ precache-manifest.559465c2964e17ad0c7090ed02fc148a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9a87bb973f44e0d128490286c2e669",
+    "url": "/MonsterProject/index.html"
+  },
+  {
+    "revision": "d308234e3e510d5399b6",
+    "url": "/MonsterProject/static/css/main.8c340894.chunk.css"
+  },
+  {
+    "revision": "9b2e0c749878edcf68e5",
+    "url": "/MonsterProject/static/js/2.e6de76fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterProject/static/js/2.e6de76fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d308234e3e510d5399b6",
+    "url": "/MonsterProject/static/js/main.6cbe6717.chunk.js"
+  },
+  {
+    "revision": "ee925e31cf4dd16705c5",
+    "url": "/MonsterProject/static/js/runtime-main.80f4e0c9.js"
+  }
+]);
